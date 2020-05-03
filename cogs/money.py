eb0f6dd080dff4ce7c064de009f4759ab82ae250
Arya Gummadi <56139032+Aryagummadi@users.noreply.github.com>
2020-05-02 21:05:40 -0700

    Update money.py

--- cogs/money.py
+++ cogs/money.py
@@ -47,20 +47,6 @@ class Money(commands.Cog):
             response = json.loads(response)
             await ctx.send("Bitcoin price is: $" + response['bpi']['USD']['rate'])
 
-    @commands.command(brief="Currency Exchange!", aliases=['Currency'])
-    async def currency(self, ctx, src, dest, num):
-        results = requests.get(
-            'https://free.currconv.com/api/v7/convert?q=%s_%s&compact=ultra&apiKey=cb68043f66ac479d4d8f' % (
-                src, dest)).json()
-        y = float(num) * float(results[src + "_" + dest])
-        final = round(y, 3)
-        await ctx.send(str(final))
-
-    @commands.command(brief="Checks the stock price.")
-    async def stock(self, ctx, ticker):
-        price = si.get_live_price(ticker)
-        price = round(price, 2)
-        await ctx.send('$' + str(price))
 
 
 def setup(client):
